d96342857beb02c60fbeffc6a7bd2fe43649dbcd
Marco Aurélio <marco7.m@gmail.com>
2020-12-31 01:26:56 -0300

    Create constants to make the configuration easier

--- script.py
+++ script.py
@@ -7,34 +7,46 @@ with open('word_lists/word_list_by_frequency.csv', newline='') as f:
 for i in range(len(word_list)):
     word_list[i] = word_list[i][0]
 
+# size of the image
 H_SIZE = 9*210
 V_SIZE = 9*297
 
-fnt = ImageFont.truetype('fonts/Hanzi-Pinyin-Font.top.ttf', 96)
+fnt = ImageFont.truetype('fonts/Hanzi-Pinyin-Font.top.ttf', 60)
 
-img = Image.new('RGB', (H_SIZE, V_SIZE), color = (255, 255, 255))
+img = Image.new('RGB', (H_SIZE, V_SIZE), color=(255, 255, 255))
 d = ImageDraw.Draw(img)
 
-spc_h = -120
+H_SPACING = 60
+V_SPACING = 70
+
+L_MARGIN = 72
+T_MARGIN = 70
+R_MARGIN = 170
+B_MARGIN = 170
+
+spc_h = -H_SPACING
 spc_v = 0
 spc_page = 0
+
 for i in range(len(word_list)):
-    print(i)
-    if (spc_h + 120 + 200)+50 > H_SIZE and (spc_v + 120 + 250)+50 > V_SIZE:
-        img.save('img/img_' + str(spc_page) + '.jpeg',format="JPEG")
-        img = Image.new('RGB', (H_SIZE, V_SIZE), color = (255, 255, 255))
+    print(f"hanzi: {i} | page: {spc_page}")
+
+    # if end of line and column, get new page
+    if (spc_h + H_SPACING + R_MARGIN) > H_SIZE and (spc_v + V_SPACING + B_MARGIN) > V_SIZE:
+        img.save('img/img_' + str(spc_page) + '.jpeg', format="JPEG")
+        img = Image.new('RGB', (H_SIZE, V_SIZE), color=(255, 255, 255))
         d = ImageDraw.Draw(img)
-        print(" === page: ", spc_page)
         spc_page += 1
         spc_h = 0
         spc_v = 0
 
-    elif (spc_h + 120 + 200)+50 > H_SIZE:
+    # check if needs new line
+    elif (spc_h + H_SPACING + R_MARGIN) > H_SIZE:
         spc_h = 0
-        spc_v += 120
-    else:
-        spc_h += 120
-    d.text((120+spc_h,120+spc_v), word_list[i], font=fnt, fill=(0, 0, 0))
+        spc_v += V_SPACING
 
-print(" === page: ", spc_page)
-img.save('img/img_' + str(spc_page) + '.jpeg',format="JPEG")
+    else:
+        spc_h += H_SPACING
+    d.text((L_MARGIN+spc_h, T_MARGIN+spc_v), word_list[i], font=fnt, fill=(0, 0, 0))
+
+img.save('img/img_' + str(spc_page) + '.jpeg', format="JPEG")
